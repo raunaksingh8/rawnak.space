aaf784a2113912b59a5726314d4e14beb75159c5
raunaksingh8 <imraunak5577@gmail.com>
2026-04-06 16:13:02 +0530

    fix: Update server on production

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,35 @@
 const express = require("express");
-const app= express();
+const app = express();
 const cors = require("cors");
-
-const {Pool} = require("pg");
+const { Pool } = require("pg");
 
 const pool = new Pool({
-    user:"postgres",
-    host:"localhost",
-    database:"myapp",
-    password:"postgres",
-    port:5432,
-})
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL
+}));
 app.use(express.json());
 
-app.get("/",(req,resp)=>{
-    resp.send("Backend is running");
-})
-
-app.get("/api/test", (req,res)=>{
-    res.json({message : "API is Working"});
+app.get("/", (req, resp) => {
+  resp.send("Backend is running");
 });
 
-app.get("/api/users",async(req,res)=>{
-    try{
-        const result = await pool.query("Select * from users");
-        res.json(result.rows);
-    }catch (err){
-        console.error(err);
-        res.status(500).send("Error fetching users");
-    }
+app.get("/api/test", (req, res) => {
+  res.json({ message: "API is Working" });
 });
 
-app.listen(5000, () => console.log("Server is running on port 5000"));
+app.get("/api/users", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM users");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching users");
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
